41b5405a45de8ab1cd12ae3a4a2ec9b1c348d511
Antoniosvj <118409406+Antoniosvj@users.noreply.github.com>
2024-07-21 17:51:03 -0300

    criei o arquivo apiCNBB para pegar a liturgia do dia

---
+++ apiCNBB.py
@@ -0,0 +1,29 @@
+#https://github.com/Dancrf/liturgia-diaria
+import requests
+import json
+
+#dia=30
+#mes=6
+
+liturgia = requests.get('https://liturgiadiaria.site/') #liturgia do dia
+#liturgia = requests.get(f"https://liturgiadiaria.site/?dia={dia}&mes={mes}") #liturgia do dia especifico
+
+liturgia = liturgia.json()
+print(liturgia)
+
+data = liturgia['data']
+print(data)
+liturgia_dia= liturgia['liturgia']
+print(liturgia_dia)
+cor = liturgia['cor']
+print(cor)
+dia = liturgia['dia']
+print(dia)
+primeira_leitura = liturgia['primeiraLeitura']
+print(primeira_leitura)
+salmo = liturgia['salmo']
+print(salmo)
+segunda_leitura = liturgia['segundaLeitura']
+print(segunda_leitura)
+evangelho = liturgia['evangelho']
+print(evangelho)
